5fcf7dfe307689693b9c6f8fd409c04b238be364
Gjoko-Bulovski <37840296+Gjoko-Bulovski@users.noreply.github.com>
2019-08-19 20:21:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
+import { async } from 'q';
 
 function App() {
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const [ quote, setQuote ] = useState();
+
+  const getData = async () => {
+    try{
+      let chuckJokes = await fetch(`https://api.chucknorris.io/jokes/random`);
+      let data = await chuckJokes.json();
+      setQuote(data.value);
+    }
+    catch(error) {
+      console.warn(`We have an error here: ${error}`);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="image" src="img/image.jpg" alt="chuck norris" />
+      <div className="opacity"></div>
+      <div className="container">
+        <h1>Chuck Norris Jokes</h1>
+        <button className="getjoke" onClick={getData}>Get Joke</button>
+        <p className="quote">{quote}</p>
+      </div>
     </div>
   );
 }
